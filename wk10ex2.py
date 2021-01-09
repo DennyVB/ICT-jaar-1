275ef918e0c189fc8210c80919ad2e431facbc28
DennyVB <76172058+DennyVB@users.noreply.github.com>
2021-01-09 13:35:29 +0100

    Add files via upload

--- wk10ex2.py
+++ wk10ex2.py
@@ -1,3 +1,97 @@
+def in_a_row_n_east(ch, r_start, c_start, a, n):
+    """ Checks if a character 'ch' appears n times in a row in east direction.
+        With starting point 'r_start' and 'c_start'
+    """
+
+    
+    rows = len(a)      
+    cols = len(a[0])   
+    
+    if r_start >= rows:
+        return False  
+
+
+    if c_start > cols - n:
+        return False
+    
+    for i in range(n):
+        if a[r_start][c_start+i] != ch:
+            return False
+    
+    return True
+
+
+
+def in_a_row_n_south(ch, r_start, c_start, a, n):
+    """ Checks if a character 'ch' appears n times in a row in south direction.
+        With starting point 'r_start' and 'c_start'
+    """
+
+    
+    rows = len(a)
+    cols = len(a[0])
+    
+
+
+    if r_start > rows - n:
+        return False
+    
+    if c_start >= cols:
+        return False
+    
+    for i in range(n):
+        if a[r_start+i][c_start] != ch:
+            return False
+    
+    return True
+
+
+
+def in_a_row_n_southeast(ch, r_start, c_start, a, n):
+    """ Checks if a character 'ch' appears n times in a row in southeast direction.
+        With starting point 'r_start' and 'c_start'
+    """
+    
+    rows = len(a)
+    cols = len(a[0])
+   
+
+    if r_start > rows - n:
+        return False
+    
+    if c_start > cols - n:
+        return False
+    
+    for i in range(n):
+        if a[r_start+i][c_start+i] != ch:
+            return False
+    
+    return True
+
+def in_a_row_n_northeast(ch, r_start, c_start, a, n):
+    """ Checks if a character 'ch' appears n times in a row in northeast direction.
+        With starting point 'r_start' and 'c_start'
+    """
+    
+    rows = len(a)
+    cols = len(a[0])
+
+    if r_start < n-1:
+        return False
+    
+    if c_start > cols - n:
+        return False
+
+    for i in range(n):
+        
+        if a[r_start-i][c_start+i] != ch:
+            return False
+        
+    return True
+
+
+
+
 class Board:
     """A data type representing a Connect-4 board
        with an arbitrary number of rows and columns.
@@ -40,7 +134,12 @@ class Board:
         
         self.data[self.height - 1][col] = ox
     
-   
+    def del_move(self, col):
+        for row in range(0, self.height):
+            if self.data[row][col] != " ":
+                self.data[row][col] = " "
+                return
+
         
     def set_board(self, move_string):
         """ Accepts a string of columns and places
@@ -77,17 +176,14 @@ class Board:
     def wins_for(self, ox):
         # Controleren op horizontale overwinningen
         for row in range(0, self.height):
-            for col in range(0, self.width - 3):
-                if self.data[row][col] == ox and \
-                    self.data[row][col + 1] == ox and \
-                    self.data[row][col + 2] == ox and \
-                    self.data[row][col + 3] == ox:
+            for col in range(0, self.width):
+                if in_a_row_n_east(ox, row, col, self.data, 4) == True:
+                    return True
+                elif in_a_row_n_south(ox, row, col, self.data, 4) == True:
+                    return True
+                elif in_a_row_n_northeast(ox, row, col, self.data, 4) == True:
+                    return True
+                elif in_a_row_n_southeast(ox, row, col, self.data, 4) == True:
                     return True
         
-        for col in range(0, self.width):
-            for row in range(0, self.hight - 3):
-                if  self.data[row][col] == ox and \
-                    self.data[row + 1][col] == ox and \
-                    self.data[row + 2][col] == ox and \
-                    self.data[row + 3][col] == ox:
-                        return True
+        return False
